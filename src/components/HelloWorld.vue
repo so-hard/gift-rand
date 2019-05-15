656204627b982ee126050fa34b1f4a7881785cb0
so-hard <qq1136339397@gmail.com>
2019-05-15 22:56:05 +0800

    init: 初次提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,61 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <md-field>
+      <label>用户数量</label>
+      <md-input v-model="value"></md-input>
+    </md-field>
+    <md-button class="md-plain" @click="rand">
+        check
+    </md-button>
+
+      <md-table v-model="mixArr" md-card >
+      <md-table-row slot="md-table-row" slot-scope="{item}" md-numeric >
+        <md-table-cell md-label="giftSender" md-sort-by="id" >{{ item[0] }}</md-table-cell>
+        <md-table-cell md-label="giftReceiver" md-sort-by="name" >{{ item[1] }}</md-table-cell>
+      </md-table-row>
+    </md-table>
   </div>
 </template>
 
 <script>
+import {RandArry} from '../extend/RandArry.js'
+// import { parse } from 'path';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: "HelloWorld",
+  comments: {},
+  data() {
+    return {
+      value: 0,
+      defArr: '',
+      randArr: '',
+      mixArr: []
+    }
+  },
+  methods:{
+    rand(){
+    let rand = new RandArry(this.value);
+    this.defArr = rand.defArry;
+    this.randArr = rand.compare()
+    this.mixArr = (this.defArr.map(
+      (ele,index) =>{
+        let arr2 = [];
+         arr2.push(ele,this.randArr[index])
+         return arr2
+      }
+    ))
 }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.md-field,.md-table{
+  width: 80vw;
+  margin: 5vw auto
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+
+
+
 </style>
--- src/extend/RandArry.js
+++ src/extend/RandArry.js
@@ -1,19 +1,52 @@
-# 获取随机数
 
-## RandArry.js
+// function compare(y){
+//     let rep = []
+//     defArry(6).forEach((ele,index) => {
+//         if(ele == y[index]){
+//             rep.push(index)
+//         }
+//     });
+    
+//     console.log(rep,y)
+//     if(rep.length != 0){
+//         compare(ranArray(defArry(6)))
+//     }
+//     return y, defArry()
+// }
 
-洗牌算法得到随机数组，如果数组的长度不大，很大概率得到数组跟原始数组在相同位置有相同元素。
+// const defArry = function (x){
+//     let arr = Array(x).fill(0,0,x);
+//     let len = arr.length;
+//     arr=arr.map((ele,index)=>{
+//         return index
+//     })
+//     return arr
+// }
 
-```js
+
+// let ranAray = function (arr) {
+//     let i = arr.length;
+//     while (i) {
+//         let j = Math.floor(Math.random()*i--);
+//         [arr[j], arr[i]] = [arr[i], arr[j]];
+//     }
+//     return arr
+//     console.log(arr)
+// }
+
+
+
+
 export class RandArry{
     constructor(len){
         //默认属性 数组长度
         this.len = Number(len)
         //初始化数组从小到大排列
         this.defArry = this.arryInit()
-        //洗牌算法得到的一系列数组，只有第一个数组，
+        //洗牌算法得到的一系列数组，只有一个数组是不和原始数组重复的0.0
         this.ranArrs = []
     }
+    
     arryInit(){
         let arr = Array(this.len).fill(0,0,this.len);
         arr=arr.map((ele,index)=>{
@@ -22,6 +55,7 @@ export class RandArry{
         //输出从小大排序
         return arr
     }
+
 
     ranArray() {
         let i = this.len;
@@ -42,14 +76,25 @@ export class RandArry{
                 rep.push(index)
             }
         });
+        
         // console.log(rep,y)
         //判断是否跟上一个位置相同，如果相同则again
-        //数字大性能也很棒...更不容易跟原始数组在相同位置有相同元素
+        //数字大性能也很棒...更不容易跟上一个位置相同
         if(rep.length != 0){
             this.compare(this.ranArray())
         }
         this.ranArrs.push(y)
         return this.ranArrs[0]
     }
+    
 }
-```
+// let myDate = new Date();
+
+// let now;
+// let pass;
+// let arr6 = new  RandArry(100000);
+// now = myDate.getTime()
+// console.log(arr6.compare(arr6.ranArray()))
+// pass = myDate.getTime()
+
+// console.log(pass-now)
